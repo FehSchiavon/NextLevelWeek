2d68d024d80828810057c3211c9d4ae98c1edb63
FehSchiavon <felipschiavon@gmail.com>
2020-06-03 16:46:11 -0300

    Add: new items categores

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -5,7 +5,7 @@ function populationUFs() {
     fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
         .then(res => res.json())
         .then(states => {
-
+            
             for (state of states) {
                 ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
             }
@@ -27,11 +27,14 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option> "
+    citySelect.disabled = true
+
     fetch(url)
         .then(res => res.json())
         .then(cities => {
             for (city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
             citySelect.disabled = false
         })
