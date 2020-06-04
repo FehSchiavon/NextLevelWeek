e9a73b24d5e61aa24aab3d7fb410190ba6458d31
FehSchiavon <felipschiavon@gmail.com>
2020-06-04 19:07:01 -0300

    Add: Nunjucks

--- src/server.js
+++ src/server.js
@@ -3,12 +3,18 @@ const server = express()
 
 server.use(express.static('public'))
 
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
 server.get('/', (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    res.render("index.html")
 })
 
 server.get('/create-point', (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    res.render("create-point.html")
 })
 
 server.listen(3000, function() {
