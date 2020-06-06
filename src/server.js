038b942fdc7ae51f7e3650532d036b78622b6947
FehSchiavon <felipschiavon@gmail.com>
2020-06-06 00:15:52 -0300

    #day5 complete/ Finish event

--- src/server.js
+++ src/server.js
@@ -55,7 +55,8 @@ server.post('/savepoint', (req, res) => {
 
     function afterInserData(err) {
         if(err) {
-            return console.log(err)
+            console.log(err)
+            return res.send("Erro no cadastro")
         }
 
         console.log('Cadastrado com sucesso')
@@ -69,7 +70,14 @@ server.post('/savepoint', (req, res) => {
 
 
 server.get('/search', (req, res) => {
-    db.all(`SELECT * FROM places`, function(err, rows) {
+
+    const search = req.query.search
+
+    if (search == "") {
+        return res.render("search-results.html", { total: 0 })
+    }
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if(err) {
             return console.log(err)
         }
@@ -77,7 +85,7 @@ server.get('/search', (req, res) => {
         const total = rows.length
         
         // Mostrar a pagina HTML com dados do Banco e Dados
-        res.render("search-results.html", { places: rows, total: total })
+        return res.render("search-results.html", { places: rows, total: total })
     })
 
 })
